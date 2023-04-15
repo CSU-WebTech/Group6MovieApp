3e341ad353e2799cee4c1a1530a757a8f6ac7ab6
Alejandro Olson <olson_alejandro@columbusstate.edu>
2023-04-15 03:29:36 +0000

    retaining changes

--- MovieApp/Clients/MovieClient.cs
+++ MovieApp/Clients/MovieClient.cs
@@ -1,20 +1,18 @@
 using System.Net.Http.Json;
 using MovieApp.Models;
-using Microsoft.Net.Http;
-using Newtonsoft.Json;
 
 namespace MovieApp.Clients;
 
 public class MovieClient {
 
-    private readonly HttpClient_client;
+    private readonly HttpClient _client;
 
     public MovieClient(HttpClient client) {
         _client = client;
     }
 
     public async Task<MovieResponse> GetMovieResponse() {
-        return await _client.GetFromJsonAsync<getMovieResponse>("https://api.themoviedb.org/3/movie/550?api_key=3d16f1e91053992401099962ac5650c9");
+        return await _client.GetFromJsonAsync<MovieResponse>("https://api.themoviedb.org/3/movie/popular?api_key=3d16f1e91053992401099962ac5650c9&language=en-US&page=1");
     }
 
 }
--- MovieApp/Models/MovieDatabaseResponse.cs
+++ MovieApp/Models/MovieDatabaseResponse.cs
@@ -5,16 +5,60 @@ namespace MovieApp.Models;
 //API key = 3d16f1e91053992401099962ac5650c9
 
 public record MovieResponse {
-    //original_title, poster_path, "genres": id, "name"
 
-    [JsonPropertyName('original_title')]
-    public string original_title { get; init; }
+    [JsonPropertyName("page")]
+    public int page { get; init; }
 
-    [JsonPropertyName('poster_path')]
+    // [JsonPropertyName("results")]
+    // public string results { get; init; }
+
+    [JsonPropertyName("poster_path")]
     public string poster_path { get; init; }
 
-    [JsonPropertyName('genres.name')]
-    public string genres { get; init; }
+    [JsonPropertyName("adult")]
+    public bool adult { get; init; }
+
+    [JsonPropertyName("overview")]
+    public string overview { get; init; }
+
+    [JsonPropertyName("release_date")]
+    public string release_date { get; init; }
+
+    [JsonPropertyName("genre_ids")]
+    public int[] genre_ids { get; init; }
+
+    [JsonPropertyName("id")]
+    public int id { get; init; }
+
+    [JsonPropertyName("original_title")]
+    public string original_title { get; init; }
+
+    [JsonPropertyName("original_language")]
+    public string original_language { get; init; }
+
+    [JsonPropertyName("title")]
+    public string title { get; init; }
+
+    [JsonPropertyName("backdrop_path")]
+    public string backdrop_path { get; init; }
+
+    [JsonPropertyName("popularity")]
+    public int popularity { get; init; }
+
+    [JsonPropertyName("vote_count")]
+    public int vote_count { get; init; }
+
+    [JsonPropertyName("video")]
+    public bool video { get; init; }
+
+    [JsonPropertyName("vote_average")]
+    public int vote_average { get; init; }
+
+    [JsonPropertyName("total_results")]
+    public int total_results { get; init; }
+
+    [JsonPropertyName("total_pages")]
+    public int total_pages { get; init; }
 
 }
 
