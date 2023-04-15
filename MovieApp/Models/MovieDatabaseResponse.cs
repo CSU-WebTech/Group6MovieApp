eb57e7e218f06eb531a5edd8adbaeac4ecf56f01
Alejandro Olson <olson_alejandro@columbusstate.edu>
2023-04-15 04:01:40 +0000

    response

--- MovieApp/Models/MovieDatabaseResponse.cs
+++ MovieApp/Models/MovieDatabaseResponse.cs
@@ -10,19 +10,10 @@ public record MovieResponse {
     public int page { get; init; }
 
     // [JsonPropertyName("results")]
-    // public string results { get; init; }
+    // public string[] results { get; init; }
 
     [JsonPropertyName("poster_path")]
     public string poster_path { get; init; }
-
-    [JsonPropertyName("adult")]
-    public bool adult { get; init; }
-
-    [JsonPropertyName("overview")]
-    public string overview { get; init; }
-
-    [JsonPropertyName("release_date")]
-    public string release_date { get; init; }
 
     [JsonPropertyName("genre_ids")]
     public int[] genre_ids { get; init; }
@@ -30,26 +21,14 @@ public record MovieResponse {
     [JsonPropertyName("id")]
     public int id { get; init; }
 
-    [JsonPropertyName("original_title")]
-    public string original_title { get; init; }
-
-    [JsonPropertyName("original_language")]
-    public string original_language { get; init; }
-
     [JsonPropertyName("title")]
     public string title { get; init; }
 
     [JsonPropertyName("backdrop_path")]
     public string backdrop_path { get; init; }
 
-    [JsonPropertyName("popularity")]
-    public int popularity { get; init; }
-
     [JsonPropertyName("vote_count")]
     public int vote_count { get; init; }
-
-    [JsonPropertyName("video")]
-    public bool video { get; init; }
 
     [JsonPropertyName("vote_average")]
     public int vote_average { get; init; }
