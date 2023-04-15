d57980140a45211930a3dcc1ba524440da8a821c
Alejandro Olson <olson_alejandro@columbusstate.edu>
2023-04-15 21:34:50 +0000

    Separate design and API GET protocol

--- MovieApp/Models/MovieDatabaseResponse.cs
+++ MovieApp/Models/MovieDatabaseResponse.cs
@@ -9,26 +9,26 @@ public record MovieResponse {
     // [JsonPropertyName("results")]
     // public string results { get; init; }
 
-    [JsonPropertyName("poster_path")]
-    public string poster_path { get; init; }
+    // [JsonPropertyName("poster_path")]
+    // public string poster_path { get; init; }
 
-    [JsonPropertyName("genre_ids")]
-    public int[] genre_ids { get; init; }
+    // [JsonPropertyName("genre_ids")]
+    // public int[] genre_ids { get; init; }
 
-    [JsonPropertyName("id")]
-    public int id { get; init; }
+    // [JsonPropertyName("id")]
+    // public int id { get; init; }
 
-    [JsonPropertyName("title")]
-    public string title { get; init; }
+    // [JsonPropertyName("title")]
+    // public string title { get; init; }
 
-    [JsonPropertyName("backdrop_path")]
-    public string backdrop_path { get; init; }
+    // [JsonPropertyName("backdrop_path")]
+    // public string backdrop_path { get; init; }
 
-    [JsonPropertyName("vote_average")]
-    public double vote_average { get; init; }
+    // [JsonPropertyName("vote_average")]
+    // public double vote_average { get; init; }
 
-    [JsonPropertyName("total_results")]
-    public int total_results { get; init; }
+    // [JsonPropertyName("total_results")]
+    // public int total_results { get; init; }
 
 }
 
