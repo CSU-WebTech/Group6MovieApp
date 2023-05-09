560bdd7e5dc2c5c262b39e5f576597c384a1a17e
Alejandro Olson <olson_alejandro@columbusstate.edu>
2023-05-09 22:23:56 +0000

    button variance and colors

--- MovieApp/Models/MovieDatabaseResponse.cs
+++ MovieApp/Models/MovieDatabaseResponse.cs
@@ -2,33 +2,31 @@ using System.Text.Json.Serialization;
 
 namespace MovieApp.Models;
 
-//API key = 3d16f1e91053992401099962ac5650c9
-
 public record MovieResponse {
 
-    // [JsonPropertyName("results")]
-    // public string results { get; init; }
+    [JsonPropertyName("results")]
+    public string? results { get; init; }
 
-    // [JsonPropertyName("poster_path")]
-    // public string poster_path { get; init; }
+    [JsonPropertyName("poster_path")]
+    public string? poster_path { get; init; }
 
-    // [JsonPropertyName("genre_ids")]
-    // public int[] genre_ids { get; init; }
+    [JsonPropertyName("genre_ids")]
+    public int[] genre_ids { get; init; }
 
-    // [JsonPropertyName("id")]
-    // public int id { get; init; }
+    [JsonPropertyName("id")]
+    public int? id { get; init; }
 
-    // [JsonPropertyName("title")]
-    // public string title { get; init; }
+    [JsonPropertyName("title")]
+    public string? title { get; init; }
 
-    // [JsonPropertyName("backdrop_path")]
-    // public string backdrop_path { get; init; }
+    [JsonPropertyName("backdrop_path")]
+    public string? backdrop_path { get; init; }
 
-    // [JsonPropertyName("vote_average")]
-    // public double vote_average { get; init; }
+    [JsonPropertyName("vote_average")]
+    public double? vote_average { get; init; }
 
-    // [JsonPropertyName("total_results")]
-    // public int total_results { get; init; }
+    [JsonPropertyName("total_results")]
+    public int? total_results { get; init; }
 
 }
 
