bcbb874a2aa14d10e0a203ebbf8a255205cfce08
Ahalya Gogulamudi <gogulamudi_ahalya@columbusstate.edu>
2023-05-09 16:35:25 +0000

    Update ContactUs page

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MovieApp;
 using MovieApp.Clients;
-using Smart.Blazor;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddHttpClient<MovieClient>();
-builder.Services.AddSmart();
 
 await builder.Build().RunAsync();
